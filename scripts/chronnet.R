31909b29c2cf16e995296b77b29c7299e074a2af
Kate Noel <noel0047@umn.edu>
2021-04-06 10:20:46 -0500

    added source code for chronnet method from https://github.com/lnferreira/chronnets/blob/master/code/chronnets.R

---
+++ scripts/chronnet.R
@@ -0,0 +1,71 @@
+# load packages
+library(igraph)
+library(dplyr)
+library(plyr)
+library(zoo)
+library(parallel)
+
+# =================================================================================
+# Creates a chronnet from a temporal data set. This method accepts multiple events
+#   in the same period of time. Dataset should be a data frame with two columns:
+#       cell: The id of the region where an event occurred
+#       t: time
+# =================================================================================
+chronnet_create <- function(dataset, self_loops=TRUE, mode="directed",  num_cores=2) {
+  time_seq = sort(unique(dataset$t))
+  if (length(time_seq) < 2)
+    stop("The total time interval in the dataset should be larger than two.")
+  max_num_cores = num_cores:1
+  num_cores = max(max_num_cores[which((max_num_cores*2) <= length(time_seq))])
+  time_list = split(time_seq, ceiling(seq_along(time_seq)/ceiling(length(time_seq)/num_cores)))
+  links = mclapply(time_list, function(time_list_part){
+    time_combs = rollapply(time_list_part, 2, function(x) x)
+    connections = apply(time_combs, 1, function(x){
+      cells_before = dataset[dataset$t == x[1], "cell"]
+      cells_after = dataset[dataset$t == x[2], "cell"]
+      links_part = as.data.frame(expand.grid(cells_before, cells_after))
+      links_part$weight = 1
+      links_part
+    })
+    connections = do.call(rbind, connections)
+    names(connections) = c("from", "to", "weight")
+    connections
+  }, mc.cores = num_cores)
+  if (length(time_list) > 1){
+    for (i in 2:length(time_list)){
+      time1 = tail(time_list[[i-1]], 1)
+      time2 = head(time_list[[i]], 1)
+      cells_before = dataset[dataset$t == time1, "cell"]
+      cells_after = dataset[dataset$t == time2, "cell"]
+      connections = as.data.frame(expand.grid(cells_before, cells_after))
+      connections$weight = 1
+      names(connections) = c("from", "to", "weight")
+      links[[i=1]] = rbind(links[[i=1]], connections)
+    }
+  }
+  links = do.call(rbind, links)
+  links = links %>% group_by(from, to) %>% tally()
+  names(links) = mapvalues(names(links), "n", "weight")
+  net = graph.empty()
+  if (nrow(links)){
+    net = graph_from_data_frame(links, directed = TRUE)
+    if (!self_loops)
+      net = simplify(net, remove.multiple = FALSE)
+    if (mode == "undirected")
+      net = as.undirected(net, edge.attr.comb = "sum")
+  } else {
+    warning("Empty graph returned")
+  } 
+  net
+}
+
+# =================================================================================
+# Prune network
+# =================================================================================
+prune_net <- function(net, prune_edge_count, undirected=FALSE){
+  if (undirected)
+    net = as.undirected(net, edge.attr.comb = "sum")
+  net = delete.edges(net, which(E(net)$weight <= prune_edge_count))
+  net_list = decompose(net)
+  net_list[[which.max(sapply(net_list, vcount))]]
+}
